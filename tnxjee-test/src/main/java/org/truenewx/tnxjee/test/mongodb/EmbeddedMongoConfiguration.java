0f9847af3a4cc5ec049a14dca96c392461d4a310
jiangnanyuzi <jiangnanyuzi@163.com>
2019-08-21 10:08:01 +0800

    添加单元测试支持模块

---
+++ tnxjee-test/src/main/java/org/truenewx/tnxjee/test/mongodb/EmbeddedMongoConfiguration.java
@@ -0,0 +1,37 @@
+package org.truenewx.tnxjee.test.mongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+
+/**
+ * 内嵌的MongoDB配置
+ *
+ * @author jianglei
+ */
+@TestConfiguration
+public class EmbeddedMongoConfiguration extends AbstractMongoConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    protected String getDatabaseName() {
+        return this.env.getProperty("spring.data.mongodb.database", "test");
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        MongoServer mongoServer = new MongoServer(new MemoryBackend());
+        mongoServer.bind();
+        return new MongoClient(new ServerAddress(mongoServer.getLocalAddress()));
+    }
+
+}
