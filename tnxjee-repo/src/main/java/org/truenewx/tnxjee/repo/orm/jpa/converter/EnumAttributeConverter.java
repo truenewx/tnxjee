4eaed739b1eaba47b12450879cef169a73b9cf8a
jiangnanyuzi <jiangnanyuzi@163.com>
2019-09-02 13:35:55 +0800

    添加JPA枚举属性转换器

---
+++ tnxjee-repo/src/main/java/org/truenewx/tnxjee/repo/orm/jpa/converter/EnumAttributeConverter.java
@@ -0,0 +1,34 @@
+package org.truenewx.tnxjee.repo.orm.jpa.converter;
+
+import javax.persistence.AttributeConverter;
+
+import org.truenewx.tnxjee.core.enums.support.EnumValueHelper;
+import org.truenewx.tnxjee.core.util.ClassUtil;
+
+/**
+ * 枚举属性转换器
+ *
+ * @author jianglei
+ */
+public class EnumAttributeConverter<T extends Enum<T>> implements AttributeConverter<T, String> {
+
+    private Class<T> entityClass;
+
+    protected Class<T> getEnumClass() {
+        if (this.entityClass == null) {
+            this.entityClass = ClassUtil.getActualGenericType(getClass(), 0);
+        }
+        return this.entityClass;
+    }
+
+    @Override
+    public String convertToDatabaseColumn(T attribute) {
+        return EnumValueHelper.getValue(attribute);
+    }
+
+    @Override
+    public T convertToEntityAttribute(String dbData) {
+        return EnumValueHelper.valueOf(getEnumClass(), dbData);
+    }
+
+}
