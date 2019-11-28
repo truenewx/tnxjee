8cf7045f406ce18a3294e75ff503e25d0d36f13c
jiangnanyuzi <jiangnanyuzi@163.com>
2019-11-28 15:45:01 +0800

    静态资源访问映射优化至框架中

---
+++ tnxjee-web-view/src/main/java/org/truenewx/tnxjee/web/view/resource/ResourceUrlConfiguration.java
@@ -0,0 +1,46 @@
+package org.truenewx.tnxjee.web.view.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+import org.truenewx.tnxjee.core.Strings;
+import org.truenewx.tnxjee.web.view.util.WebViewPropertyConstant;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 资源路径配置
+ */
+@Configuration
+public class ResourceUrlConfiguration {
+
+    private static final String DEFAULT_RESOURCES_STATIC_PATTERN = "/assets/**,/vendor/**,**/*.css,**/*.js,/robots.txt";
+
+    @Autowired
+    private Environment environment;
+
+    @Bean("resourceUrlHandlerMapping")
+    @ConditionalOnMissingBean(value = AbstractUrlHandlerMapping.class, name = "resourceUrlHandlerMapping", search = SearchStrategy.CURRENT)
+    public SimpleUrlHandlerMapping resourceUrlHandlerMapping(ResourceHttpRequestHandler handler) {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        Map<String, ResourceHttpRequestHandler> urlMap = new LinkedHashMap<>();
+        String staticPattern = this.environment.getProperty(WebViewPropertyConstant.RESOURCES_STATIC_PATTERN, DEFAULT_RESOURCES_STATIC_PATTERN);
+        String[] patterns = staticPattern.split(Strings.COMMA);
+        for (String pattern : patterns) {
+            pattern = pattern.trim();
+            urlMap.put(pattern, handler);
+        }
+        mapping.setUrlMap(urlMap);
+        mapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return mapping;
+    }
+
+}
--- tnxjee-web-view/src/main/java/org/truenewx/tnxjee/web/view/util/WebViewPropertyConstant.java
+++ tnxjee-web-view/src/main/java/org/truenewx/tnxjee/web/view/util/WebViewPropertyConstant.java
@@ -8,9 +8,16 @@ public class WebViewPropertyConstant {
     private WebViewPropertyConstant() {
     }
 
+    private static final String PREFIX = "tnxjee.web.view.";
+
     /**
      * 业务异常错误消息展示页面路径
      */
-    public static final String ERROR_PATH_BUSINESS = "tnxjee.web.view.error.path.business";
+    public static final String ERROR_PATH_BUSINESS = PREFIX + "error.path.business";
+
+    /**
+     * 静态资源路径样式模板
+     */
+    public static final String RESOURCES_STATIC_PATTERN = PREFIX + "resources.static-pattern";
 
 }
