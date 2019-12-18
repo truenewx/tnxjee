0aae8357dd69d0400215b9c862ac4b4a42c3871a
jiangnanyuzi <jiangnanyuzi@163.com>
2019-12-18 08:20:38 +0800

    格式优化

--- tnxjee-web-controller/src/main/java/org/truenewx/tnxjee/web/controller/spring/security/config/annotation/web/configuration/WebSecurityConfigurerSupport.java
+++ tnxjee-web-controller/src/main/java/org/truenewx/tnxjee/web/controller/spring/security/config/annotation/web/configuration/WebSecurityConfigurerSupport.java
@@ -1,19 +1,13 @@
 package org.truenewx.tnxjee.web.controller.spring.security.config.annotation.web.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.util.Assert;
-import org.springframework.web.servlet.HandlerMapping;
 import org.truenewx.tnxjee.core.spring.util.SpringUtil;
 import org.truenewx.tnxjee.web.controller.spring.security.access.WebAccessDecisionManager;
 import org.truenewx.tnxjee.web.controller.spring.security.user.UserAuthenticationService;
@@ -46,7 +40,8 @@ public abstract class WebSecurityConfigurerSupport extends WebSecurityConfigurer
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserAuthenticationService userAuthenticationService = SpringUtil.getFirstBeanByClass(getApplicationContext(), UserAuthenticationService.class);
+        UserAuthenticationService userAuthenticationService = SpringUtil.getFirstBeanByClass(getApplicationContext(),
+                UserAuthenticationService.class);
         Assert.notNull(userAuthenticationService, "Can not find bean of type: UserAuthenticationService");
         auth.userDetailsService(userAuthenticationService);
         auth.authenticationProvider(userAuthenticationService);
