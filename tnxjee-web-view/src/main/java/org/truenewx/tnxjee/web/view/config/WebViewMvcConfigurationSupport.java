2600812e5a2cf1e7e8142c70ff972dee62bbf8f5
jiangnanyuzi <jiangnanyuzi@163.com>
2019-12-31 17:00:27 +0800

    使用fastjson序列化ResponseBody结果

--- tnxjee-web-view/src/main/java/org/truenewx/tnxjee/web/view/config/WebViewMvcConfigurationSupport.java
+++ tnxjee-web-view/src/main/java/org/truenewx/tnxjee/web/view/config/WebViewMvcConfigurationSupport.java
@@ -1,15 +1,18 @@
 package org.truenewx.tnxjee.web.view.config;
 
-import java.util.function.Consumer;
-
-import javax.servlet.DispatcherType;
-
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
 import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.truenewx.tnxjee.web.controller.http.converter.FastJsonHttpMessageConverter;
 import org.truenewx.tnxjee.web.view.servlet.filter.ForbidAccessFilter;
 import org.truenewx.tnxjee.web.view.sitemesh.config.BuildableSiteMeshFilter;
+
+import javax.servlet.DispatcherType;
+import java.util.function.Consumer;
 
 /**
  * WEB视图层MVC配置支持
@@ -32,6 +35,15 @@ public abstract class WebViewMvcConfigurationSupport {
         frb.addUrlPatterns("/*");
         frb.setDispatcherTypes(DispatcherType.FORWARD, DispatcherType.REQUEST, DispatcherType.ERROR);
         return frb;
+    }
+
+    @Bean
+    public HttpMessageConverters httpMessageConverters() {
+        FastJsonHttpMessageConverter fastJsonConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastJsonConverter);
     }
 
 }
