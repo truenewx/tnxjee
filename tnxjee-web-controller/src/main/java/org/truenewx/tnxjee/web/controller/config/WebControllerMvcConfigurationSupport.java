eebbe4126af046c2ee0cef3d60186409068cb5ba
jiangnanyuzi <jiangnanyuzi@163.com>
2020-01-13 20:50:10 +0800

    OpenSession更改为OpenEntityManager

--- tnxjee-web-controller/src/main/java/org/truenewx/tnxjee/web/controller/config/WebControllerMvcConfigurationSupport.java
+++ tnxjee-web-controller/src/main/java/org/truenewx/tnxjee/web/controller/config/WebControllerMvcConfigurationSupport.java
@@ -1,8 +1,15 @@
 package org.truenewx.tnxjee.web.controller.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.truenewx.tnxjee.web.controller.context.request.CompositeOpenSessionInViewInterceptor;
+import org.truenewx.tnxjee.repo.jpa.support.JpaSchemaTemplate;
+import org.truenewx.tnxjee.repo.support.SchemaTemplate;
+import org.truenewx.tnxjee.web.controller.context.request.DynamicOpenEntityManagerInViewInterceptor;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Collection;
 
 /**
  * WEB控制层MVC配置支持，可选的控制层配置均在此配置支持体系中
@@ -11,15 +18,20 @@ import org.truenewx.tnxjee.web.controller.context.request.CompositeOpenSessionIn
  */
 public abstract class WebControllerMvcConfigurationSupport implements WebMvcConfigurer {
 
-    public CompositeOpenSessionInViewInterceptor compositeOpenSessionInViewInterceptor() {
-        return new CompositeOpenSessionInViewInterceptor();
-    }
+    @Autowired
+    private ApplicationContext context;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        CompositeOpenSessionInViewInterceptor interceptor = compositeOpenSessionInViewInterceptor();
-        if (interceptor.isAvailable()) {
-            registry.addWebRequestInterceptor(interceptor);
+        Collection<SchemaTemplate> templates = this.context.getBeansOfType(SchemaTemplate.class).values();
+        for (SchemaTemplate template : templates) {
+            if (template instanceof JpaSchemaTemplate) {
+                JpaSchemaTemplate jst = (JpaSchemaTemplate) template;
+                EntityManagerFactory entityManagerFactory = jst.getEntityManager().getEntityManagerFactory();
+                DynamicOpenEntityManagerInViewInterceptor interceptor = new DynamicOpenEntityManagerInViewInterceptor();
+                interceptor.setEntityManagerFactory(entityManagerFactory);
+                registry.addWebRequestInterceptor(interceptor);
+            }
         }
     }
 
