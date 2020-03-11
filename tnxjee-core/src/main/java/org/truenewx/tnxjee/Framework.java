c2bc17bdcd5574150bfa97e84284fd87521d8f23
jiangnanyuzi <jiangnanyuzi@163.com>
2020-03-11 22:14:49 +0800

    解决框架中的组件不自动加载的问题

--- tnxjee-core/src/main/java/org/truenewx/tnxjee/Framework.java
+++ tnxjee-core/src/main/java/org/truenewx/tnxjee/Framework.java
@@ -1,14 +1,11 @@
 package org.truenewx.tnxjee;
 
-import org.springframework.context.annotation.ComponentScan;
-
 /**
  * 框架信息
  *
  * @author jianglei
  * @since JDK 1.8
  */
-@ComponentScan(basePackageClasses = Framework.class)
 public class Framework {
 
     /**
