76eaf0ebbdadb86b3e1adb381281e33804876db4
jiangnanyuzi <jiangnanyuzi@163.com>
2020-03-11 22:20:24 +0800

    恢复包扫描配置

--- tnxjee-core/src/main/java/org/truenewx/tnxjee/Framework.java
+++ tnxjee-core/src/main/java/org/truenewx/tnxjee/Framework.java
@@ -1,11 +1,14 @@
 package org.truenewx.tnxjee;
 
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * 框架信息
  *
  * @author jianglei
  * @since JDK 1.8
  */
+@ComponentScan(basePackageClasses = Framework.class)
 public class Framework {
 
     /**
