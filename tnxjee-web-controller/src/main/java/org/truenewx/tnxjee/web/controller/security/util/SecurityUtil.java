d2bc923b25e01793e3053e451a7e66cf18ee3912
jianglei <jianglei1@ut.cn>
2020-03-12 17:27:03 +0800

    添加安全工具类

---
+++ tnxjee-web-controller/src/main/java/org/truenewx/tnxjee/web/controller/security/util/SecurityUtil.java
@@ -0,0 +1,56 @@
+package org.truenewx.tnxjee.web.controller.security.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.truenewx.tnxjee.model.spec.user.security.UserSpecificDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * 安全工具类
+ */
+public class SecurityUtil {
+
+    private SecurityUtil() {
+    }
+
+    /**
+     * 获取已获得的授权清单，匿名用户也会拥有匿名的授权清单
+     *
+     * @return 已获得的授权清单
+     */
+    public static Collection<? extends GrantedAuthority> getGrantedAuthorities() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (context != null) {
+            Authentication authentication = context.getAuthentication();
+            if (authentication != null) {
+                return authentication.getAuthorities();
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * 获取已授权的当前用户，匿名用户将返回null
+     *
+     * @param <U> 用户类型
+     * @return 已授权的当前用户
+     */
+    public static <U extends UserSpecificDetails<?>> U getAuthorizedUser() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (context != null) {
+            Authentication authentication = context.getAuthentication();
+            if (authentication != null) {
+                Object principal = authentication.getPrincipal();
+                if (principal instanceof UserSpecificDetails) {
+                    return (U) principal;
+                }
+            }
+        }
+        return null;
+    }
+
+}
