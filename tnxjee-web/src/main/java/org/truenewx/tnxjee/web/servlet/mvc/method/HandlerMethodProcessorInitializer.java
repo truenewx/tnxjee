665baed11c266e59f884396402ed3f692afc1183
jiangnanyuzi <jiangnanyuzi@163.com>
2020-03-31 22:16:43 +0800

    转义机制改进

--- tnxjee-web/src/main/java/org/truenewx/tnxjee/web/servlet/mvc/method/HandlerMethodProcessorInitializer.java
+++ tnxjee-web/src/main/java/org/truenewx/tnxjee/web/servlet/mvc/method/HandlerMethodProcessorInitializer.java
@@ -1,14 +1,18 @@
 package org.truenewx.tnxjee.web.servlet.mvc.method;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.annotation.RequestParamMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.mvc.method.annotation.PathVariableMethodArgumentResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestAttributeMethodArgumentResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 /**
  * 处理器方法处理器初始化器，负责将自定义的方法处理器加载到容器中
@@ -16,17 +20,19 @@ import java.util.List;
 @Component
 public class HandlerMethodProcessorInitializer {
 
+    private Class<?>[] argumentResolvers = { RequestResponseBodyMethodProcessor.class,
+            RequestParamMethodArgumentResolver.class, RequestAttributeMethodArgumentResolver.class,
+            PathVariableMethodArgumentResolver.class };
+    private Class<?>[] returnValueHandlers = { RequestResponseBodyMethodProcessor.class };
+
     @Autowired
     public void setRequestMappingHandlerAdapter(RequestMappingHandlerAdapter adapter) {
-        EscapeRequestResponseBodyMethodProcessor bodyProcessor = null;
-
         List<HandlerMethodArgumentResolver> resolvers = adapter.getArgumentResolvers();
         if (resolvers != null) {
             List<HandlerMethodArgumentResolver> delegatedResolvers = new ArrayList<>();
             for (HandlerMethodArgumentResolver resolver : resolvers) {
-                if (resolver instanceof RequestResponseBodyMethodProcessor) {
-                    bodyProcessor = new EscapeRequestResponseBodyMethodProcessor((RequestResponseBodyMethodProcessor) resolver);
-                    delegatedResolvers.add(bodyProcessor);
+                if (ArrayUtils.contains(this.argumentResolvers, resolver.getClass())) {
+                    delegatedResolvers.add(new EscapeHandlerMethodArgumentResolver(resolver));
                 } else {
                     delegatedResolvers.add(resolver);
                 }
@@ -38,8 +44,8 @@ public class HandlerMethodProcessorInitializer {
         if (handlers != null) {
             List<HandlerMethodReturnValueHandler> delegatedHandlers = new ArrayList<>();
             for (HandlerMethodReturnValueHandler handler : handlers) {
-                if (handler instanceof RequestResponseBodyMethodProcessor) {
-                    delegatedHandlers.add(bodyProcessor);
+                if (ArrayUtils.contains(this.returnValueHandlers, handler.getClass())) {
+                    delegatedHandlers.add(new EscapeHandlerMethodReturnValueHandler(handler));
                 } else {
                     delegatedHandlers.add(handler);
                 }
