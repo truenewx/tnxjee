2f77ba845c5575df4730288243afedc6f543d580
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-02 17:06:27 +0800

    完成JPA数据源配置简化

--- tnxjee-repo-jpa/src/main/java/org/truenewx/tnxjee/repo/jpa/config/RepoJpaConfiguration.java
+++ tnxjee-repo-jpa/src/main/java/org/truenewx/tnxjee/repo/jpa/config/RepoJpaConfiguration.java
@@ -1,9 +1,8 @@
 package org.truenewx.tnxjee.repo.jpa.config;
 
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
@@ -14,20 +13,21 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.jta.JtaTransactionManager;
+import org.truenewx.tnxjee.core.Strings;
 import org.truenewx.tnxjee.core.util.LogUtil;
-import org.truenewx.tnxjee.repo.jpa.jdbc.datasource.embedded.EmbeddedDataSourceFactoryBean;
 import org.truenewx.tnxjee.repo.jpa.support.JpaAccessTemplate;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -42,36 +42,52 @@ public class RepoJpaConfiguration extends JpaBaseConfiguration {
 
     @Autowired
     private HibernateProperties hibernateProperties;
-    @Autowired
     private ApplicationContext context;
 
-    public RepoJpaConfiguration(DataSource dataSource,
+    /**
+     * 在多数据源场景下，可创建子类，构造函数中指定DataSourceProperties和DataSource的beanName
+     */
+    public RepoJpaConfiguration(ApplicationContext context,
+            DataSourceProperties dataSourceProperties,
+            DataSource dataSource,
             JpaProperties properties,
             ObjectProvider<JtaTransactionManager> jtaTransactionManager,
             ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
         super(dataSource, properties, jtaTransactionManager, transactionManagerCustomizers);
+        this.context = context;
+        // 本配置会在数据源对象构建完之后，在数据源初始化之前加载，spring-data-jpa框架会检查数据库表结构，
+        // 此时如果数据库表结构未初始化，则会报错退出，导致后续的数据库初始化脚本配置无法正常执行，
+        // SpringBoot的数据源初始化器Configuration的可见性是默认，无法通过@AutoConfigureAfter指定加载顺序，
+        // 所以需要在此时提前执行数据库初始化脚本，以确保数据库表结构检查通过
+        initDataSource(dataSourceProperties);
+    }
+
+    private void initDataSource(DataSourceProperties properties) {
+        List<String> locations = new ArrayList<>();
+        List<String> schema = properties.getSchema();
+        if (schema != null) {
+            locations.addAll(schema);
+            schema.clear(); // 清除以避免框架再次执行
+        }
+        List<String> data = properties.getData();
+        if (data != null) {
+            locations.addAll(data);
+            data.clear(); // 清除以避免框架再次执行
+        }
+        if (locations.size() > 0) {
+            DataSourceInitializer initializer = new DataSourceInitializer();
+            initializer.setDataSource(getDataSource());
+            ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+            locations.forEach(location -> {
+                populator.addScript(this.context.getResource(location));
+            });
+            initializer.setDatabasePopulator(populator);
+            initializer.afterPropertiesSet();
+        }
     }
 
     protected String getSchema() {
         return null;
-    }
-
-    public DataSource embeddedDataSource(String... scriptLocations) throws Exception {
-        EmbeddedDataSourceFactoryBean factory = new EmbeddedDataSourceFactoryBean();
-        factory.setDatabaseType(EmbeddedDatabaseType.H2);
-        String schema = getSchema();
-        if (StringUtils.isNotBlank(schema)) {
-            factory.setDatabaseName(schema);
-        }
-        if (ArrayUtils.isNotEmpty(scriptLocations)) {
-            Resource[] resources = new Resource[scriptLocations.length];
-            for (int i = 0; i < scriptLocations.length; i++) {
-                resources[i] = new ClassPathResource(scriptLocations[i]);
-            }
-            factory.setScripts(resources);
-        }
-        factory.afterPropertiesSet();
-        return factory.getObject();
     }
 
     @Override
@@ -92,15 +108,32 @@ public class RepoJpaConfiguration extends JpaBaseConfiguration {
 
     protected void addMappingResources(List<String> mappingResources) {
         try {
-            Resource[] resources = this.context.getResources("classpath*:META-INF/jpa/*.xml");
+            String location = getMappingLocation();
+            // 确保目录以/开头和结尾
+            if (!location.startsWith(Strings.SLASH)) {
+                location = Strings.SLASH + location;
+            }
+            if (!location.endsWith(Strings.SLASH)) {
+                location += Strings.SLASH;
+            }
+            Resource[] resources = this.context.getResources("classpath*:" + location + "*.xml");
             for (Resource resource : resources) {
                 String path = resource.getURI().toString();
-                int index = path.lastIndexOf("/META-INF/jpa/");
+                int index = path.lastIndexOf(location);
                 mappingResources.add(path.substring(index + 1));
             }
         } catch (IOException e) {
             LogUtil.error(getClass(), e);
         }
+    }
+
+    /**
+     * 获取实体映射配置文件所在目录，相对于classpath
+     *
+     * @return 实体映射配置文件所在目录
+     */
+    protected String getMappingLocation() {
+        return "/META-INF/jpa/";
     }
 
     @Override
