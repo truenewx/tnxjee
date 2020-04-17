8b7e64d5ef27e95c31166610d4a13699d74f280d
jiangnanyuzi <jiangnanyuzi@163.com>
2020-04-17 21:09:58 +0800

    使用属性配置资源映射

--- tnxjee-web/src/main/java/org/truenewx/tnxjee/web/config/WebMvcConfigurationSupport.java
+++ tnxjee-web/src/main/java/org/truenewx/tnxjee/web/config/WebMvcConfigurationSupport.java
@@ -5,19 +5,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.truenewx.tnxjee.web.cors.CorsRegistryProperties;
+import org.truenewx.tnxjee.web.resource.ResourceMappingProperties;
+
+import java.util.Map;
 
 /**
  * WEB MVC配置支持，可选的控制层配置均在此配置支持体系中
  *
  * @author jianglei
  */
-@EnableConfigurationProperties(CorsRegistryProperties.class)
+@EnableConfigurationProperties({ ResourceMappingProperties.class, CorsRegistryProperties.class })
 public abstract class WebMvcConfigurationSupport implements WebMvcConfigurer {
 
     @Autowired(required = false)
+    private ResourceMappingProperties resourceMappingProperties;
+
+    @Autowired(required = false)
     private CorsRegistryProperties corsRegistryProperties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (this.resourceMappingProperties != null) {
+            Map<String, String[]> mapping = this.resourceMappingProperties.getMapping();
+            if (mapping != null) {
+                mapping.forEach((pattern, locations) -> {
+                    registry.addResourceHandler(pattern)
+                            .addResourceLocations(locations);
+                });
+            }
+        }
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
