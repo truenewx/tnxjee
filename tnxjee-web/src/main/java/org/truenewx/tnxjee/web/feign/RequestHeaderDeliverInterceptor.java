0ad477fe4d025c204fab9b7b94e3d7867f48686a
projavaer <pro_javaer@163.com>
2020-05-22 12:10:37 +0800

    Feign请求中已有的头信息不被改动传递

--- tnxjee-web/src/main/java/org/truenewx/tnxjee/web/feign/RequestHeaderDeliverInterceptor.java
+++ tnxjee-web/src/main/java/org/truenewx/tnxjee/web/feign/RequestHeaderDeliverInterceptor.java
@@ -1,8 +1,9 @@
 package org.truenewx.tnxjee.web.feign;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
-import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,19 +21,26 @@ import feign.RequestTemplate;
 public class RequestHeaderDeliverInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
+        // 所有Feign请求均加上内部RPC请求标志
+        template.header(WebConstants.HEADER_INTERNAL_RPC, Boolean.TRUE.toString());
+
         HttpServletRequest request = SpringWebContext.getRequest();
         if (request != null) {
+            Map<String, Collection<String>> feignHeaders = template.headers();
             Enumeration<String> headerNames = request.getHeaderNames();
             while (headerNames.hasMoreElements()) {
                 String headerName = headerNames.nextElement();
-                Enumeration<String> headers = request.getHeaders(headerName);
-                List<String> headerValues = new ArrayList<>();
-                while (headers.hasMoreElements()) {
-                    headerValues.add(headers.nextElement());
+                // Feign头信息中未包含的才传递，以避免Feign创建的头信息被改动
+                if (!feignHeaders.containsKey(headerName)) {
+                    Enumeration<String> requestHeaders = request.getHeaders(headerName);
+                    Collection<String> headerValues = new ArrayList<>();
+                    while (requestHeaders.hasMoreElements()) {
+                        headerValues.add(requestHeaders.nextElement());
+                    }
+                    feignHeaders.put(headerName, headerValues);
                 }
-                template.header(headerName, headerValues);
             }
-            template.header(WebConstants.HEADER_INTERNAL_RPC, Boolean.TRUE.toString());
+            template.headers(feignHeaders);
         }
     }
 }
