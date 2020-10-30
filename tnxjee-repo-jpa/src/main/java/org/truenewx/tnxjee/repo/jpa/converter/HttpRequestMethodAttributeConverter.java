3d683d212e81ba8151837bdd4cb131bddfc52310
jianglei <jianglei81@sina.cn>
2020-10-30 15:21:51 +0800

    添加http请求方法属性转换器

---
+++ tnxjee-repo-jpa/src/main/java/org/truenewx/tnxjee/repo/jpa/converter/HttpRequestMethodAttributeConverter.java
@@ -0,0 +1,9 @@
+package org.truenewx.tnxjee.repo.jpa.converter;
+
+import org.truenewx.tnxjee.core.spec.HttpRequestMethod;
+
+/**
+ * http请求方法的属性转换器
+ */
+public class HttpRequestMethodAttributeConverter extends EnumAttributeConverter<HttpRequestMethod> {
+}
