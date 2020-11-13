1ac597642431047042eee80c4f0f74f56b008e30
jianglei <jianglei81@sina.cn>
2020-11-13 11:32:28 +0800

    内部JWT配置支持抽取到框架层面

---
+++ tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/CoreConfigurerSupport.java
@@ -0,0 +1,43 @@
+package org.truenewx.tnxjee.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.truenewx.tnxjee.core.util.function.ProfileSupplier;
+
+/**
+ * 核心配置支持
+ */
+public abstract class CoreConfigurerSupport {
+
+    @Autowired
+    private ProfileSupplier profileSupplier;
+
+    @Bean
+    public InternalJwtConfiguration internalJwtConfiguration() {
+        String profile = this.profileSupplier.get();
+        int expiredTimeSeconds = getInternalJwtExpiredTimeSeconds(profile);
+        return new InternalJwtConfiguration(getInternalJwtSecretKey(), expiredTimeSeconds);
+    }
+
+    /**
+     * 获取指定运行环境中的内部JWT过期时间秒数
+     *
+     * @param profile 运行环境
+     * @return 内部JWT过期时间秒数
+     */
+    protected int getInternalJwtExpiredTimeSeconds(String profile) {
+        switch (profile) {
+            case "local":
+                return 1000;
+            case "test":
+                return 100;
+        }
+        return 10;
+    }
+
+    /**
+     * @return 内部JWT密钥
+     */
+    protected abstract String getInternalJwtSecretKey();
+
+}
