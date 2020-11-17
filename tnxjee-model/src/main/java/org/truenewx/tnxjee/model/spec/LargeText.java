749607a5622c19dc0b55cc2d7bac82f50220a9a3
jianglei <jianglei81@sina.cn>
2020-11-17 21:38:56 +0800

    主键类型更改为字符串

--- tnxjee-model/src/main/java/org/truenewx/tnxjee/model/spec/LargeText.java
+++ tnxjee-model/src/main/java/org/truenewx/tnxjee/model/spec/LargeText.java
@@ -5,24 +5,25 @@ import org.truenewx.tnxjee.model.entity.unity.Unity;
 /**
  * 大文本单体。用于将一般业务实体中的大文本字段独立出来，以使其获得更好的性能表现
  */
-public class LargeText implements Unity<Long> {
+public class LargeText implements Unity<String> {
 
-    private Long id;
+    private String id;
     private String value;
 
     public LargeText() {
     }
 
-    public LargeText(String value) {
+    public LargeText(String id, String value) {
+        this.id = id;
         this.value = value;
     }
 
     @Override
-    public Long getId() {
+    public String getId() {
         return this.id;
     }
 
-    protected void setId(Long id) {
+    protected void setId(String id) {
         this.id = id;
     }
 
