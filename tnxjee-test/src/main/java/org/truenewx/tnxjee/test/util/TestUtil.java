ac62bd231f28a07ed3ebe420ebdfcb8ed6052a3c
jianglei <jianglei81@sina.cn>
2020-12-07 14:51:30 +0800

    添加模拟对象的支持

--- tnxjee-test/src/main/java/org/truenewx/tnxjee/test/util/TestUtil.java
+++ tnxjee-test/src/main/java/org/truenewx/tnxjee/test/util/TestUtil.java
@@ -1,5 +1,10 @@
 package org.truenewx.tnxjee.test.util;
 
+import java.util.function.Consumer;
+
+import org.mockito.Mockito;
+import org.truenewx.tnxjee.core.util.BeanUtil;
+
 /**
  * 单元测试工具类
  *
@@ -15,9 +20,7 @@ public class TestUtil {
     }
 
     /**
-     *
      * @return 是否单元测试环境中
-     *
      * @author jianglei
      */
     public static boolean isTesting() {
@@ -33,6 +36,24 @@ public class TestUtil {
             TESTING = false;
         }
         return TESTING;
+    }
+
+    /**
+     * 模拟指定类型的对象到指定目标对象的指定属性上，并执行模拟测试过程
+     *
+     * @param classToMock 模拟类型
+     * @param target      目标对象
+     * @param fieldName   目标属性
+     * @param consumer    测试过程
+     * @param <T>         模拟类型
+     */
+    public static <T> void mockBeanTo(Class<T> classToMock, Object target, String fieldName, Consumer<T> consumer) {
+        target = BeanUtil.getTargetSource(target);
+        T originalBean = BeanUtil.getFieldValue(target, fieldName);
+        T mockBean = Mockito.mock(classToMock);
+        BeanUtil.setFieldValue(target, fieldName, mockBean);
+        consumer.accept(mockBean);
+        BeanUtil.setFieldValue(target, fieldName, originalBean);
     }
 
 }
